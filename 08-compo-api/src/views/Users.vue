674f6870eb87dbc830661c5633523baba5b64c82
Paula Sánchez P <paula.sanchez1031@gmail.com>
2022-02-28 16:26:46 -0500

    adding format

--- 08-compo-api/src/views/Users.vue
+++ 08-compo-api/src/views/Users.vue
@@ -3,11 +3,10 @@
   <h2 v-else>Usuarios</h2>
   <h5 v-if="errorMessage">{{ errorMessage }}</h5>
   <div v-if="users.length > 0">
-    <user-list :users="users" v-slot="{user}"> <!--se desestructura el propslot user para ser usado-->
-      <h5>
-        {{ user.first_name }} {{user.last_name}}
-      </h5>
-        <span>{{user.email}}</span>
+    <user-list :users="users" v-slot="{ user }">
+      <!--se desestructura el propslot user para ser usado-->
+      <h5>{{ user.first_name }} {{ user.last_name }}</h5>
+      <span>{{ user.email }}</span>
     </user-list>
   </div>
 
